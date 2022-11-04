7fba64e74ff12601c844678dc661f30388724c6e
Goodness <babasolagoodness21@gmail.com>
2022-11-04 18:07:57 +0100

    I updated the script file

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
 
-customerDetails = []
-if(localStorage.bankDetails){
-    customerDetails =JSON.parse(localStorage.getItem('bankDetails'))
+var allStudent = [];
+if(localStorage.studentDetails){
+    allStudent = JSON.parse(localStorage.getItem('studentDetails'))
 }
 
-const createAcc = () => {
-    let regexForEmail = /^(([\w]+)([@])([\w]+)([.])([a-zA-Z]{1,5})([.][\w]{1,5})?)$/
-      let regexForName = /^[\w]{1,}$/
-      let regexForPhonenumber = /^[\d]{11}$/
-      let regexForPassword = /^[\w]{4,} [\d]{3}$/
-
-    userDetails = {
+function signUp(){
+    var newStudent = {
         firstName : firstname.value,
         lastName : lastname.value,
-        Email : useremail.value,
-        phonenumber : userphonenumber.value,
-        password : userpassword.value
+        Email : email.value,
+        phoneNumber : phonenumber.value,
+        password : Password.value
     }
     
     
-    if(regexForName.test(firstname.value) == false){
-        validateName.innerHTML = "type a correct name!"
+    if((firstName = firstname.value) && (lastName = lastname.value) && (Email = email.value) && (phoneNumber = phonenumber.value) && (password = Password.value)){
+        allStudent.push (newStudent)
+        firstname.value = ""
+        lastname.value = ""
+        email.value = ""
+        phonenumber.value = ""
+        Password.value = ""
+        window.location.href = "index  .html"
     }
-    if(regexForName.test(lastname.value)==false){
-        validateName2.innerHTML ="lastname is incorrect!"
+    else{
+        alert("PLEASE FILL THE REQUIRED SPACES")
     }
-    if(regexForEmail.test(useremail.value)== false){
-        validateEmail.innerHTML = "input the right email!"
-    }
-    if(regexForPhonenumber.test(userphonenumber.value)==false){
-        validatePhonenumber.innerHTML = "phonenumber must be 11 digits"
-    }
-    if(regexForPassword.test(userpassword.value)==false){
-        validatePassword.innerHTML = "password contain at least 4 letter and 3 digits"
-    }
-    customerDetails.push(userDetails)
-    localStorage.setItem('bankDetails', JSON.stringify(customerDetails))
-    window.location.href = "signin.html"
+    localStorage.setItem('studentDetails', JSON.stringify(allStudent))
 }
 
 
