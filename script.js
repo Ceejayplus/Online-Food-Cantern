3dde4fa3bc438cc28cfb6e2f8ace459db6c9d905
Goodness <babasolagoodness21@gmail.com>
2022-11-05 17:12:09 +0100

    I updated home.html home.css and script.js files respectively

--- script.js
+++ script.js
@@ -64,3 +64,19 @@ if(found){
     alert("INVALID")
 }
 }
+
+
+// Home Page
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+}
