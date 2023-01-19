6ddc373f77a83dd0c31ea38a1a0a5f0063f92f23
Goodness <babasolagoodness21@gmail.com>
2023-01-19 12:47:10 +0100

    I linked a file to a button in cart.js file

--- cart.js
+++ cart.js
@@ -31,7 +31,7 @@ function purchasClicked(){
     alert('Thank you for your purchase')
     var cartItems = document.getElementsByClassName('section-p1')[0]
     while(cartItems.hasChildNodes()) {
-        window.location.href = ""
+        window.location.href = "Next.html"
         // cartItems.removeChild(cartItems.firstChild)
     }
 }
